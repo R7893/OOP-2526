7766d7aff5d741f024f9f88eef16894acc8ae80e
rokos <sharkwr123@gmail.com>
2026-03-09 18:17:17 +0100

    Class student is defined.

---
+++ Fundemetals/src/pck_cls_obj/Main.java
@@ -0,0 +1,12 @@
+package pckg_cls_obj;
+
+public class AppTest{
+
+    public static void main(String[] args){
+        Student s1 = new Student(name:"Petar", surname: "Petrić");
+        Student s2 = new Student(name:"Ivan", surname: "Petrov");
+
+
+
+    }
+}
---
+++ Fundemetals/src/pck_cls_obj/Student.java
@@ -0,0 +1,24 @@
+package pck_cls_obj;
+
+public class Student {
+
+    private String name;
+
+    private String surname;
+
+    private int id;
+
+    public Student(String name, String surname, int id){
+        this.name = name;
+        this.surname = surname;
+        this.id = id;
+    }
+
+    public void subscribeToStudentService(){
+        System.out.println("Student's id: " + id);
+        System.out.println(name + " - " + surname + " is subrscribed to student service.");
+    }
+
+
+
+}
